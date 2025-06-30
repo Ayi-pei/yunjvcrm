71f100eb80a88210b30718ca8c623b2dff1b2cd9
阿义 <ayi198701@gmail.com>
2025-06-30 16:30:59 +0800

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ export const Header: React.FC = () => {
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
             <input
               type="text"
-              placeholder="Search agents, sessions..."
+              placeholder="搜索客户及密钥..."
               className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent w-80"
             />
           </div>
