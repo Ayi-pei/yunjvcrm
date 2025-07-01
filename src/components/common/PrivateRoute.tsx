d68abd1337ef58447b7954fda8b22aeee62af25e
阿义 <ayi198701@gmail.com>
2025-07-01 14:43:21 +0800

    Updated PrivateRoute.tsx

--- src/components/common/PrivateRoute.tsx
+++ src/components/common/PrivateRoute.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStore } from '../../stores/authStore';
 
+// 当前路由保护组件
 export const PrivateRoute: React.FC = () => {
   const { isAuthenticated } = useAuthStore();
 
